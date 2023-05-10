3c2ac427f8c4225ea440fccaea2fd256e8a9a7c4
gudtn2 <tyrueu@gmail.com>
2023-05-10 13:40:09 +0900

    0510

---
+++ List/List/Main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+// 1. 함수 사용
+// 2. 클래스 이해도
+
+// for(초기문; 조건문; 증감문)
+
+// ** 초기문 : int i = 0;
+// ** 조건문 : i < 10;
+// ** 증감문 : ++i
+
+#define function(x) (std::cout << x << std::endl)
+
+#define Single(T)					\
+public:								\
+	static T& GetInstance()			\
+	{								\
+		static T instance;			\
+		return instance;			\
+	}								\
+private:							\
+	T(const T&) = delete;			\
+	T& operator=(const T&) = delete;
+
+#define GetSingle(T) (T::GetInstance())
+
+class Singleton
+{
+public:
+	Single(Singleton)
+		/*
+private:
+	static Singleton* Instance;
+public:
+	static Singleton* GetInstance()
+	{
+		if (Instance == nullptr)
+			Instance = new Singleton;
+
+		return Instance;
+	}
+	*/
+private:
+	int Value;
+public:
+	int GetValue() { return Value; }
+	void SetValue(int value) { Value = value; }
+
+private:
+	Singleton() : Value(0) {}
+public:
+	~Singleton() {}
+};
+//Singleton* Singleton::Instance = nullptr;
+
+class Object
+{
+public:
+	string name;
+	int age;
+public:
+	virtual void Start(int x) = 0;
+	virtual int Update(int x) = 0;
+};
+
+class Player : public  Object
+{
+public:
+	virtual void Start(int x)
+	{
+		age = x;
+	}
+	virtual int Update(int x)override
+	{
+		if (age < x)
+			return 1;
+
+		return 0;
+	}
+};
+
+class Bullet : public  Object
+{
+public:
+	virtual void Start(int x)
+	{
+		age = x;
+	}
+	virtual int Update(int x)override
+	{
+		if (age < x)
+			return 1;
+
+		return 0;
+	}
+};
+
+int main(void)
+{
+	GetSingle(Singleton).SetValue(10);
+	cout << GetSingle(Singleton).GetValue() << endl;
+
+	/*
+	function(10);
+	Object* player = new Player;
+
+	int length = 16;
+	Object* bulletList = new Bullet[length];
+
+	for (int i = 0; i < length; ++i)
+	{
+		if (bulletList[i].Update(i))
+			cout << "Bullet age 값이" << i << "보다 작습니다" << endl;
+	}
+	int value = 10;
+
+	if (player->Update(value))
+		cout << "player age 값이 " << value << "보다 작습니다." << endl;
+		*/
+	return 0;
+}
